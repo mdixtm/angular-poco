dbc4d9dfef9b99440d8100f350ac5f963a669a3c
Madalina Tomus <madalina.tomus@accesa.eu>
2018-06-25 17:18:10 +0300

    Add functionallity for cart

---
+++ src/app/cart/cart-item.ts
@@ -0,0 +1,6 @@
+export interface ICartItem {
+    productId: number,
+    productName: string,
+    quantity: number,
+    categoryId: number,
+}
--- src/app/cart/cart-service.ts
+++ src/app/cart/cart-service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from "@angular/core";
+import { ICartItem } from "./cart-item";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+
+    private _cartItemsUrl = './api/cart/cart.json';
+
+    constructor (private _http: HttpClient) {}
+
+    getCartItems(): Observable<ICartItem[]> {
+        return this._http.get<ICartItem[]>(this._cartItemsUrl)
+            .do(data => console.log('Those are cart items' + JSON.stringify(data)))
+            .catch(this.handleError);    
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
+    }
+}
